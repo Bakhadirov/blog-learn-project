03721bdde1fad500cb72bc82685ec77d601dbdd8
Farkhad <frada1985@gmail.com>
2021-08-05 01:48:22 +0300

    first commit

---
+++ blog/models.py
@@ -0,0 +1,73 @@
+from django.db import models
+from django.urls import reverse
+
+'''
+Category
+-------
+title, slug
+
+Tag
+--------
+title, slug
+
+Post
+--------
+title, slug, author, content, created_at, photo, views, category, tags
+'''
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, verbose_name='Url', unique=True)# дублирующих записей не будет в слаг
+    # категориии
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('category', kwargs={'slug':self.slug})
+
+
+
+    class Meta:
+        verbose_name= 'Категория(ю)'
+        verbose_name_plural = 'Категории'
+        ordering = ['title']
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=255, verbose_name='Url', unique=True)
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name= 'Тэг'
+        verbose_name_plural = 'Тэги'
+        ordering = ['title']
+
+    def get_absolute_url(self):
+        return reverse('tag', kwargs={'slug':self.slug})
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, verbose_name='Url', unique=True)
+    author = models.CharField(max_length=100)
+    content = models.TextField(blank= True)
+    created_at = models.DateTimeField(auto_now_add= True, verbose_name= 'Publicated') #При создании записи будет
+    # выставлена    # текущая дата
+    photo = models.ImageField(upload_to='photos/%y/%m/%d/', blank=True)
+    views = models.IntegerField(default=0, verbose_name='number of view')
+    category = models.ForeignKey(Category, on_delete= models.PROTECT, related_name= 'posts') #указываем ссылку на
+    # модель с    # которой    # связываем,    # если объявлена после    # поста, то заключаем в кавычки.
+    tags = models.ManyToManyField(Tag, blank=True, related_name= 'posts')
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post', kwargs={'slug':self.slug})
+
+    class Meta:
+        ordering = ['-created_at'] #сортировка в обратном порядке, чтобы самые свежие были вначале
+        verbose_name= 'Статья(ю)'
+        verbose_name_plural = 'Статьи'
